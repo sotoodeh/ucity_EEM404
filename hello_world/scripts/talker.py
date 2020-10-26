7d0f6f8e338dc3677acaf230b24ef5c4f88aa4d5
sotoodeh <54372537+sotoodeh@users.noreply.github.com>
2020-10-26 19:37:13 +0000

    Update talker.py

--- hello_world/scripts/talker.py
+++ hello_world/scripts/talker.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import String
+import rospy                       # rospy has all the important ROS functions
+from std_msgs.msg import String    # importing a string message type in Python
+                                   # We have to use package_name.msg and import the required message type
 
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10)  # 10hz
-    while not rospy.is_shutdown():
+    pub = rospy.Publisher('chatter', String, queue_size=10)   # To publish a topic called chatter with a std_msgs/String message type
+                                                              # and a queue_size of 10.
+    rospy.init_node('talker', anonymous=True)                 # The first argument is the name of the node ('talker'), 
+                                                              # and the second argument is anonymous=True, which means the node can run on multiple instances.
+    rate = rospy.Rate(10)                                     # 10hz After creating the instance, we have to call the sleep() function inside it 
+                                                              # to get the rate in effect.
+    while not rospy.is_shutdown():                            # creating a loop to publish a msg
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
-        rate.sleep()
+        rate.sleep()                                         # to get the rate in effect.
 
 
 if __name__ == '__main__':
     try:
-        talker()
+        talker()                                             # talker function 
     except rospy.ROSInterruptException:
         pass
