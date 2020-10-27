6c2e8b974862887e25e9e2ede4f114c75a9e7cbb
sotoodeh <54372537+sotoodeh@users.noreply.github.com>
2020-10-27 13:39:55 +0000

    Update talker.cpp

--- hello_world/src/talker.cpp
+++ hello_world/src/talker.cpp
@@ -24,14 +24,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-// %Tag(FULLTEXT)%
-// %Tag(ROS_HEADER)%
-#include "ros/ros.h"
-// %EndTag(ROS_HEADER)%
-// %Tag(MSG_HEADER)%
-#include "std_msgs/String.h"
-// %EndTag(MSG_HEADER)%
 
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 #include <sstream>
 
 /**
